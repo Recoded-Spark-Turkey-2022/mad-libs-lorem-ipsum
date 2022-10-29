20a55da5174350b538e2b88404094210c2e03cab
naji-alhusami <naji.alhusami@bahcesehir.edu.tr>
2022-10-29 13:20:06 +0300

    first edit

--- madlibs.js
+++ madlibs.js
@@ -1,18 +1,18 @@
 /**
  * Complete the implementation of parseStory.
- * 
+ *
  * parseStory retrieves the story as a single string from story.txt
  * (I have written this part for you).
- * 
+ *
  * In your code, you are required (please read this carefully):
  * - to return a list of objects
  * - each object should definitely have a field, `word`
  * - each object should maybe have a field, `pos` (part of speech)
- * 
+ *
  * So for example, the return value of this for the example story.txt
  * will be an object that looks like so (note the comma! periods should
  * be handled in the same way).
- * 
+ *
  * Input: "Louis[n] went[v] to the store[n], and it was fun[a]."
  * Output: [
  *  { word: "Louis", pos: "noun" },
@@ -22,21 +22,83 @@
  *  { word: "store", pos: "noun" }
  *  { word: "," }
  *  ....
- * 
+ *
  * There are multiple ways to do this, but you may want to use regular expressions.
  * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
  */
 function parseStory(rawStory) {
-  // Your code here.
-  return {}; // This line is currently wrong :)
+  const noun = /\[n\]/; // regular Expression for [n]
+  const verb = /\[v\]/; // regular Expression for [v]
+  const adj = /\[a\]/; // regular Expression for [a]
+  const outputStory = []; // new empty erray to push each word of our story
+  const ourStory = rawStory.split(" "); //convert the story to array using split()
+  ourStory.map((story) => {
+    // looping over the array
+    if (noun.test(story) === true) {
+      //checking if the [n] exists in each word in our story
+      outputStory.push({
+        // push each story to the emtpy array
+        word: story,
+        pos: "noun",
+      });
+    } else if (verb.test(story) === true) {
+      //checking if the [v] exists in each word in our story
+      outputStory.push({
+        // push each story to the emtpy array
+        word: story,
+        pos: "verb",
+      });
+    } else if (adj.test(story) === true) {
+      //checking if the [a] exists in each word in our story
+      outputStory.push({
+        // push each story to the emtpy array
+        word: story,
+        pos: "adj",
+      });
+    } else {
+      outputStory.push({
+        // push each story to the emtpy array
+        word: story,
+      });
+    }
+  });
+  return outputStory;
 }
 
 /**
  * All your other JavaScript code goes here, inside the function. Don't worry about
  * the `then` and `async` syntax for now.
- * 
+ *
  * You'll want to use the results of parseStory() to display the story on the page.
  */
-getRawStory().then(parseStory).then((processedStory) => {
-  console.log(processedStory);
-});
+
+getRawStory()
+  .then(parseStory)
+  .then((processedStory) => {
+    console.log(processedStory);
+    createElements(processedStory);
+  });
+
+function createElements(outputStory) {
+  const madEdit = document.querySelector(".madLibsEdit"); //select the element with class (madLibsEdit)
+  const madPrev = document.querySelector(".madLibsPreview"); //select the element with class (madLibsPreview)
+
+  outputStory.map((story) => { //looping over the array of objects that we return in line 57
+    if ("pos" in story === false) { //checking if each object has "pos" key
+      //madEdit.textContent = madEdit.textContent + `${story.word} `;
+      //madPrev.textContent = madEdit.textContent + `${story.word} `;
+      madEdit.innerHTML = madEdit.innerHTML + `${story.word} `;
+      madPrev.innerHTML = madEdit.innerHTML + `${story.word} `;
+    } else if ("pos" in story === true) { //checking if each object has "pos" key
+      //for (let i = 0; i < outputStory.length; i++) {
+       // const newElEdit = document.createElement("input");
+        //madEdit.appendChild(newElEdit);
+        //const newElPrev = document.createElement("input");
+        //madPrev.appendChild(newElPrev);
+        madEdit.innerHTML = madEdit.innerHTML + `<input />`;
+      madPrev.innerHTML = madEdit.innerHTML + `<input />`;
+    //}
+    }
+  });
+  return outputStory;
+}
